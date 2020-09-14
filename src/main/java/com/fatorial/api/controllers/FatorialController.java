0d08aa8af37a6c527b5e4e7d4887cd08629b3f57
higorpo <higorpiresoliveira@gmail.com>
2020-09-14 19:08:49 +0000

    :sparkles: Create the API

---
+++ src/main/java/com/fatorial/api/controllers/FatorialController.java
@@ -0,0 +1,26 @@
+package com.fatorial.api.controllers;
+
+import java.math.BigInteger;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+
+@RestController
+public class FatorialController {
+    @GetMapping(value = "/factorial", produces = MediaType.APPLICATION_JSON_VALUE)
+    String compute(@RequestParam Integer number) {
+        return "{\"result\": \""+ factorial(number).toString() +"\"}";
+    }
+
+    public static BigInteger factorial(int number) { 
+        BigInteger factorial = BigInteger.ONE; 
+
+        for (int i = number; i > 0; i--) { 
+            factorial = factorial.multiply(BigInteger.valueOf(i)); 
+        } 
+
+        return factorial; 
+    }
+}
